2ac17f8d75996ff40a2635f92714f7fee10d4585
gulnisademir <134624631+gulnisademirr@users.noreply.github.com>
2023-08-23 22:16:45 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import './App.css';
+import Quote from "./components/Quote";
+import Button from "./components/Button";
+
+const App = () => {
+    const [quote, setQuote] = useState([]);
+    const [color, setColor] = useState("#000000");
+
+    const fetchQuote = async () => {
+        const response = await fetch("https://api.quotable.io/random");
+        const randomQuote = await response.json();
+        setQuote(randomQuote);
+        setColor("#" + Math.floor(Math.random() * 16777).toString(16));
+
+console.log(response);
+    };
+    // React.useEffect(() => {
+        useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    const handleClick = () => {
+        return fetchQuote();
+    };
+
+    return (
+        <div className="container" style={{ backgroundColor: color }}>
+            <wrapper id="quote-box">
+                <Quote
+                    content={quote.content}
+                    author={quote.author}
+                    color={color}
+                />
+                <div id="footer">
+                    <a href="https://twitter.com" id="tweet-quote">
+                    <FontAwesomeIcon icon={faXTwitter} size="lg" />
+                    </a>
+                    <Button handleClick={handleClick} />
+                </div>
+            </wrapper>
+           
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+export default function Quote(props) {
+    return (
+        <div className="main">
+          
+                <h1 id="text">
+                <i className="quote_icon">
+                <FontAwesomeIcon icon={faQuoteLeft} />
+                </i>
+                {props.content}
+                <i className="quote_icon">
+                <FontAwesomeIcon icon={faQuoteRight} />
+                </i>
+            </h1>
+            
+
+            <p style={{ color: props.color }} id="author">
+                -{props.author}
+            </p>
+        </div>
+    );
+}
